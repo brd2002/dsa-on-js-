05eb4e4a0a08c676b6d6a265e15c0e2ae1eb367c
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2025-04-11 19:40:10 +0530

    lecture 5 on array done

--- Array/lecture.1.js
+++ Array/lecture.1.js
@@ -1,3 +1,14 @@
 /**
  * ! Array lecture 1
+ * ? data structure is how data should be represented in memory
+ * * Array actually store data in a linear contiguous orientation in the memory 
+ * * Js array support hetrogenous data -> string , number , boolean can be stored
+ * * String in js is immutable type 
+ * * Array in js is mutable
+ * ! lecture done 4 , 5 , 
  */
+let x = [1, 2, 3, 4, 5 , "what is your name"];
+let str = "bharat"
+
+// console.log(x);
+console.log(str.length);
